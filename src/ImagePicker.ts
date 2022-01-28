b50c1264754dfe6c6595d1e47662595c1ca5bd69
Hugo Gresse <hugo.gresse@gmail.com>
2022-01-28 09:52:26 +0100

    Change default light theme color on image picker and verify file types before upload

--- src/ImagePicker.ts
+++ src/ImagePicker.ts
@@ -10,6 +10,8 @@ export type ImagePickEvent = Event & {
     }
 }
 
+const SUPPORTED_IMAGE_FILE_TYPES = ["image/jpg", "image/jpeg", "image/png"]
+
 export class ImagePicker extends LitElement {
     static styles = css`
     :host {
@@ -26,6 +28,7 @@ export class ImagePicker extends LitElement {
         display: flex;
         align-items: center;
         justify-content: center;
+        background-color: #EEE;
     }
 
     file-drop span {
@@ -39,7 +42,7 @@ export class ImagePicker extends LitElement {
     file-drop.drop-valid label, label:hover {
         background-color: green;
         outline-offset: -15px;
-        background-color: #666;
+        background-color: #DDD;
     }
 
     file-drop.drop-invalid label {
@@ -47,6 +50,15 @@ export class ImagePicker extends LitElement {
     }
 
     @media (prefers-color-scheme: dark) {
+
+        label {
+            background-color: #666;
+        }
+
+        label:hover {
+            background-color: #333;
+        }
+
         file-drop label {
             outline-color: #888;
         }
@@ -60,12 +72,17 @@ export class ImagePicker extends LitElement {
     @property({type: Boolean}) inlineMode: boolean = false
 
     __onFileDrop(e: FileDropEvent) {
-        this._dispatchFiles(e.files)
+        if(e.files.length) {
+            this._dispatchFiles(e.files)
+        }
     }
 
     __onFileUpload(e: Event) {
         if(e.target instanceof HTMLInputElement && e.target.files) {
-            this._dispatchFiles(Array.from(e.target.files))
+            const acceptedFiles = Array.from(e.target.files).filter(file => SUPPORTED_IMAGE_FILE_TYPES.includes(file.type))
+            if(acceptedFiles.length) {
+                this._dispatchFiles(acceptedFiles)
+            }
         }
     }
 
@@ -78,7 +95,7 @@ export class ImagePicker extends LitElement {
     }
 
     render() {
-
+        const accept = SUPPORTED_IMAGE_FILE_TYPES.join(", ") +  ", .png, .jpg, .jpeg, .JPG, .JPEG"
         const fileDropClass = this.inlineMode ? "inline" : ""
 
         const labelContent = this.inlineMode ? html`<svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="#535559"><path d="M5,3A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H14.09C14.03,20.67 14,20.34 14,20C14,19.32 14.12,18.64 14.35,18H5L8.5,13.5L11,16.5L14.5,12L16.73,14.97C17.7,14.34 18.84,14 20,14C20.34,14 20.67,14.03 21,14.09V5C21,3.89 20.1,3 19,3H5M19,16V19H16V21H19V24H21V21H24V19H21V16H19Z" /></svg>` : html`<strong>Choose an image</strong><span> or drag it here</span>.`
@@ -97,7 +114,7 @@ export class ImagePicker extends LitElement {
                     type="file"
                     placeholder="or click here"
                     multiple
-                    accept=".png, .jpg, .jpeg, .JPG, .JPEG, image/jpg, image/jpeg, image/png"
+                    accept=${accept}
                     @change="${this.__onFileUpload}"
                 >
             </file-drop>
